1fab571ff74eb65d2d1b349e3a8729e105bb145d
gracielacesareo <graciela.cesareoc@gmail.com>
2022-12-05 11:02:24 -0600

    first commit

---
+++ src/components/GiftList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div v-for="item in list" :key="item.id">
+        {{ item.name }}
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            list:[
+                {
+                    name: 'Earphones',
+                    id:1,
+                    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                },
+                {
+                    name: 'Jacket',
+                    id:2,
+                    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                },
+                {
+                    name: 'Backpack',
+                    id:3,
+                    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+                },
+
+            ]
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
